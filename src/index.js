20157fd247e43db401ce810b828e513bc3426908
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-01-21 14:32:27 +0200

    21/01

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { fetchImg } from './js/fatch-img'
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-let incrementPage = 1;
+let incrementPage;
+let totalHits = 0;
 
 Notify.init({
   timeout: 2000,
@@ -17,32 +17,36 @@ const refs = {
   formRef: document.getElementById('search-form'),
   LoadMoreRef: document.querySelector('.load-more'),
 }
+
 refs.formRef.addEventListener('submit',onSearch);
 
 function onSearch(e) {
   e.preventDefault()
-
+  refs.LoadMoreRef.classList.add('is-hidden')
   refs.galleryRef.innerHTML = "";
   incrementPage = 1
+  totalHits = 0
   const inputData = e.currentTarget.searchQuery.value.trim();
-  fetchImg(inputData, incrementPage).then(({data}) => data).then(onValidationTotalImg)
-
-  refs.LoadMoreRef.addEventListener('click', () => {
-    incrementPage += 1;
-    fetchImg(inputData, incrementPage).then(({data}) => data).then(onValidationTotalImg)
-  })
   
+  fetchImg(inputData, incrementPage).then(({data}) => {
+    Notify.info(`Hooray! We found ${data.totalHits} images.`)
+  
+    return data}).then(onValidationTotalImg)
+
+  refs.LoadMoreRef.addEventListener('click', ()=>{onLoadMore(inputData)});
+
 }
 
-function onMarkupGallery(imgArray) {
-  console.log(imgArray);
-  refs.LoadMoreRef.classList.toggle('is-hidden')
-  if (imgArray.length === 0) {
-    Notify.failure(`❌ Sorry, there are no images matching your search query. Please try again.`);
-  return
+function onLoadMore(inputData) {
+    incrementPage += 1;
+    fetchImg(inputData, incrementPage).then(({data}) => data).then(onValidationTotalImg)
+
   }
-  refs.galleryRef.innerHTML = imgArray.map(({webformatURL,tags, likes, views, comments, downloads, largeImageURL}) => `<div class="photo-card">
-  <a class="gallery__item" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+
+function onMarkupGallery(imgArray) {
+
+  const markupGallery = imgArray.map(({webformatURL,tags, likes, views, comments, downloads, largeImageURL}) => `<div class="photo-card">
+  <a class="gallery__item" href="${largeImageURL}"><img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes:</b>${likes}</p>
@@ -51,30 +55,31 @@ function onMarkupGallery(imgArray) {
     <p class="info-item">
       <b>Comments</b>${comments}</p>
     <p class="info-item">
-      <b>Downloads</b>${downloads}</p></div></div>`).join('');
+      <b>Downloads</b>${downloads}</p></div></div></a>`).join('');
+    refs.galleryRef.insertAdjacentHTML('beforeend', markupGallery)
   const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captions: true,
   captionsData: 'alt'
-})
-      refs.LoadMoreRef.classList.remove('is-hidden')
-  galleryRef.addEventListener('click', currentImg)
+}).refresh();
 
 }
-
 
   const onValidationTotalImg = (data) => {
-    if (data.totalHits === 0) {
-    Notify.failure(`Were sorry, but you've reached the end of search results.`);
-    }
-    onMarkupGallery(data.hits)
-  }
-
-  function currentImg(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
+    if (data.hits.length === 0) {
+      Notify.failure(`❌ Sorry, there are no images matching your search query. Please try again.`);
         return
-    }
-    const imgOriginalLink = e.target.parentNode.href;
+      }
+    totalHits += data.hits.length
+    console.log(data);
+    console.log("🚀 ~ data.totalHits", data.totalHits);
+  if (data.totalHits === totalHits) {
+   refs.LoadMoreRef.classList.add('is-hidden')
+    Notify.info(`Were sorry, but you've reached the end of search results.`)
+  }else{
+    refs.LoadMoreRef.classList.remove('is-hidden')
+      }
+    onMarkupGallery(data.hits)
+  };
 
-}
+
--- src/js/fatch-img.js
+++ src/js/fatch-img.js
@@ -1,32 +1,12 @@
 import axios from "axios";
 export async function fetchImg(inputData, incrementPage) {
-    const BASE_URL = 'https://pixabay.com/api/'
-    const KEY = 'key=32970540-84e885805fcb13ea237a5964c'
-    return await axios.get(`${BASE_URL}?${KEY}&q=${inputData}&image_type=photo&orientation=horizontal&safesearch=true&page=${incrementPage}&per_page=40`);
+  console.log("🚀 ~ 4", inputData);
+  
+const BASE_URL = 'https://pixabay.com/api/'
+const KEY = 'key=32970540-84e885805fcb13ea237a5964c'
+return await axios.get(`${BASE_URL}?${KEY}&q=${inputData}&image_type=photo&orientation=horizontal&safesearch=true&page=${incrementPage}&per_page=40`);
   
   }
 
 
-
-
-
-
-
-
-
-// export async function fetchImg(inputData,incrementPage) {
-//     console.log(inputData);
-
-//     const BASE_URL = 'https://pixabay.com/api/'
-//     const KEY = 'key=32970540-84e885805fcb13ea237a5964c'
-//     try {
-
-//       const response = await axios.get(`${BASE_URL}?${KEY}&q=${inputData}&image_type=photo&orientation=horizontal&safesearch=true&page=${incrementPage}&per_page=40`);
-//       const array = await response.json();
-//       console.log(array);
-//     }catch (error) {
-//         console.error(error);
-//       }
-
-// };
 
